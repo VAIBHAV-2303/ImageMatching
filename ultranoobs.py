a59c5bf37084dcc67e2a4d8cc4a98e76d2c0e531
Vaibhav <vaibhav.garg@students.iiit.ac.in>
2019-04-19 14:45:03 +0530

    Minute

--- ultranoobs.py
+++ ultranoobs.py
@@ -2,6 +2,7 @@ import matplotlib.image as mpimg
 import glob
 import numpy as np
 import math
+from scipy import ndimage
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
@@ -14,18 +15,30 @@ slidespath.sort()
 
 slides = []
 
-# Converting to rgb and then normalizing
+# Converting to rgb, applying sobel and then normalizing
 for i in slidespath:
 	im = mpimg.imread(i)
 	gray = rgb2gray(im)
-	gray = gray.astype(np.int32)
+
+	dx = ndimage.sobel(gray, 1)
+	dy = ndimage.sobel(gray, 0)
+	gray = np.hypot(dx, dy)
+	gray *= 255.0/np.max(gray)
+
 	slides.append((gray-np.mean(gray))/math.sqrt(np.var(gray)))
-	    
+
+correct = 0
+incorrect = 0
 
 for i in range(len(framespath)):
 	im = mpimg.imread(framespath[i])
 	gray = rgb2gray(im)
-	gray = gray.astype(np.int32)
+
+	dx = ndimage.sobel(gray, 1)
+	dy = ndimage.sobel(gray, 0)
+	gray = np.hypot(dx, dy)
+	gray *= 255.0/np.max(gray)
+
 	gray = (gray-np.mean(gray))/math.sqrt(np.var(gray))
 
 	curmax = -1e15
@@ -33,9 +46,20 @@ for i in range(len(framespath)):
 	for j in range(len(slides)):
 		try:
 			temp = np.sum(np.multiply(gray, slides[j]))
-			if temp > curmax:
+			if temp >= curmax:
 				curmax = temp
 				ans = j
 		except:
 			pass
-	print(framespath[i].split('/')[3], slidespath[ans].split('/')[3])
+
+	
+	if (framespath[i].split('/')[3].split('_')[0] + '_' + framespath[i].split('/')[3].split('_')[1]) == slidespath[ans].split('/')[3].split('.')[0]:
+		correct += 1
+	else:
+		print(framespath[i].split('/')[3], slidespath[ans].split('/')[3])
+		incorrect += 1
+
+print('=================================================')
+print('Correct: ', correct, 'Incorrect: ', incorrect)
+print('Accuracy: ', 100*correct/(correct+incorrect))
+print('=================================================')
